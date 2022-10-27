21943cdfbb1853a5e77a8031b0eea70107cafe2d
Serhii Kvasha <seregeykvasha@gmail.com>
2022-10-27 12:08:09 +0200

    create an exercise model

---
+++ models/Exercise.model.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
+
+const exerciseSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required."],
+  },
+  category: {
+    type: String,
+    required: [true, "Provide the category"],
+  },
+  type: {
+    type: String,
+    enum: [
+      "cardio",
+      "strength",
+      "balance",
+      "flexibility",
+      "coordination",
+      "warm-up",
+      "other",
+    ],
+    default: "other",
+    required: [true, "Choose the type of exercise"],
+  },
+  intensity: {
+    type: String,
+    enum: ["low", "moderate", "high"],
+    required: [true, "Choose the intensity of an exercise"],
+  },
+  muscle: {
+    type: String,
+    enum: ["chest", "back", "arms", "abdominals", "legs", "shoulders", "other"],
+    required: [true, "Choose one"],
+  },
+  specificArea: {
+    type: String,
+  },
+  duration: {
+    type: Number,
+  },
+  timeUnit: {
+    type: String,
+    enum: ["minutes", "hours"],
+  },
+  imageUrl: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+});
+
+module.exports = model("Exercise", exerciseSchema);
---
+++ routes/exercise.routes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const mongoose = require("mongoose");
+
+const Exercise = require("../models/Exercise.model");
+
+//router.post("/exercises",(req,res,next)=>{
+//    const {}
+//})
