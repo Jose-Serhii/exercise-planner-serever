1824db53c2e1dbbe5c04372dd87a56393796c390
UndefinedGrunt <hassler.leonard@live.com>
2022-10-27 18:23:34 +0200

    created basic plan route

--- routes/plan.routes.js
+++ routes/plan.routes.js
@@ -1,8 +1,11 @@
-const router = require("express").Router();
-const { response } = require("../app");
-const ExerciseModel = require("../models/Exercise.model");
+const PlanModel = require("../models/Plan.model");
 const Plan = require("../models/Plan.model");
+const router = require("express").Router()
 
+
+
+
+// GET CREATE
 router.post("/plans", (req, res, next) => {
   const { day, date, activities, description } = req.body;
   const newPlan = {
@@ -23,4 +26,74 @@ router.post("/plans", (req, res, next) => {
     });
 });
 
+
+//GET ALL PLANS
+router.get("/plans", (req, res, next) => {
+  Plan.find()
+    .populate({ path: "activities.exercise" })
+    .then((allPlans) => {
+      res.json(allPlans);
+      console.log(allPlans);
+    })
+
+    .catch((err) => {
+      console.log("error getting list of plans...", err);
+      res.status(500).json({
+        message: "error getting list of plans",
+        error: err,
+      });
+    });
+
+});
+
+
+//GET PLANS BY ID
+router.get('/plans/:planId', (req, res, next) => {
+  const { planId } = req.params;
+
+
+  Plan.findById(planId)
+    .then((plan) => res.json(plan))
+    .catch((err) => {
+      console.log("error getting plan details...", err);
+      res.status(500).json({
+        message: "error getting plan details...",
+        error: err
+      })
+    });
+});
+
+//UPDATE PLANS
+router.put('/plans/:planId', (req, res, next) => {
+  const { planId } = req.params;
+
+
+  Plan.findByIdAndUpdate(planId, req.body, { new: true })
+    .then((updatedPlan) => res.json(updatedPlan))
+    .catch(err => {
+      console.log("error updating plan...", err);
+      res.status(500).json({
+        message: "error updating plan...",
+        error: err
+      })
+    });
+
+  //DELETE PLANS
+  router.delete('/plans/:planId', (req, res, next) => {
+    const { planId } = req.params;
+
+
+    Plan.findByIdAndRemove(planId)
+      .then(() => res.json({ message: `Plan with ${planId} is removed successfully.` }))
+      .catch(err => {
+        console.log("error deleting plan...", err);
+        res.status(500).json({
+          message: "error deleting plan...",
+          error: err
+        })
+      });
+  });
+
+
+});
 module.exports = router;
